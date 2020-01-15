b0cdbc4414c6f2ab4b2195c9e9716823f19073da
Dbyu85 <50050452+Dbyu85@users.noreply.github.com>
2020-01-15 13:33:21 +0100

    linking emailjs with contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+	emailjs.send("gmail", "rosie", {
+		"form_name": contactForm.fullname.value,
+		"form_email": contactForm.emailaddress.value,
+		"project_request": contactForm.projectsummary.value
+	})
+	.then(
+		function(response) {
+			console.log("SUCCESS", response);
+		},
+		function(error) {
+			console.log("FAILED", error);
+		});
+		
+}
